e2edfe543d4c272ef84ce77d1484a6d55fcc0c9b
dobeedoo <cielo.alegam@cit.edu>
2024-05-27 02:06:29 +0800

    ud
    asd

--- src/data/sideBarData.jsx
+++ src/data/sideBarData.jsx
@@ -11,50 +11,6 @@ import {
 
 export const SideBarData = [
   {
-    title: "Home",
-    path: "/",
-    role: "ALL",
-    icon: <FontAwesomeIcon icon={faHouse} style={{ fontSize: "15px" }} />,
-  },
-  {
-    title: "View Profile",
-    path: "/viewProfile",
-    roles: "EMPLOYEE",
-    icon: <FontAwesomeIcon icon={faUser} style={{ fontSize: "15px" }} />,
-  },
-  {
-    title: "Take Evaluation",
-    path: "/takeEvaluation",
-    role: "EMPLOYEE",
-    icon: <FontAwesomeIcon icon={faFileLines} style={{ fontSize: "15px" }} />,
-  },
-  {
-    title: "View Ratings",
-    path: "/viewRatings",
-    role: "EMPLOYEE",
-    icon: <FontAwesomeIcon icon={faStar} style={{ fontSize: "15px" }} />,
-  },
-  {
-    title: "Manage Account",
-    path: "/manageAccount",
-    role: "ADMIN",
-    icon: <FontAwesomeIcon icon={faUsers} style={{ fontSize: "15px" }} />,
-  },
-  {
-    title: "Manage Offices",
-    path: "/manageOffices",
-    role: "ADMIN",
-    icon: (
-      <FontAwesomeIcon icon={faBuildingUser} style={{ fontSize: "15px" }} />
-    ),
-  },
-  {
-    title: "Manage Employee",
-    path: "/manageEmployee",
-    role: "ADMIN",
-    icon: <FontAwesomeIcon icon={faUserTie} style={{ fontSize: "15px" }} />,
-  },
-  {
     title: "View Profile",
     path: "/ViewProfileAdmin",
     role: "HEAD",
