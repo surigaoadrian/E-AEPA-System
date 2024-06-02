86afbe52f446a6a0b2f3e5b01438a431180e5c0b
angelay123 <153171088+angelay123@users.noreply.github.com>
2024-06-02 17:48:13 +0800

    modified:   src/modals/ViewResults.jsx

--- src/modals/ViewResults.jsx
+++ src/modals/ViewResults.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Box, Typography, Divider, Paper, Table, TableHead,TableBody, TableCell, TableRow, TableContainer, Tabs, Tab } from '@mui/material';
 import Chart from 'react-apexcharts';
 import Matrix from "../modals/Matrix";
